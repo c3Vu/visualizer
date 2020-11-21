ed6d70f1679cc6f5ed99d3675030b197eccf94a5
Jingyi Lu <jl11525@nyu.edu>
2020-11-22 00:39:41 +0800

    init

---
+++ src/components/CodeContainer/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.css";
+
+export default function CodeContainer(props) {
+  return <div className="CodeContainer">我是文本编辑器</div>;
+}
---
+++ src/components/TopBar/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+export default class TopBar extends React.Component {
+  render() {
+    return <div className="TopBar">Hello</div>;
+  }
+}
